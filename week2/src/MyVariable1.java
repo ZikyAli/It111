0dc3e0b5db230c535995680c8572db5bc00eb905
ZikyAli <166438367+ZikyAli@users.noreply.github.com>
2024-04-26 02:38:48 -0700

    Add files via upload

---
+++ week2/src/MyVariable1.java
@@ -0,0 +1,11 @@
+public class MyVariable1 {
+
+    public static void main(String[] args) {
+
+        String name = "Ziky";
+         int hours = 40;
+
+        System.out.println(name);
+        System.out.println(hours);
+    }
+}
